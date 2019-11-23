1130f88bf05381d5fa757d08d3bd5180aa50c2f1
Josh Faison <Joshfaiso@yahoo.com>
2019-11-23 09:39:22 -0500

    functional product

--- password.js
+++ password.js
@@ -1,19 +1,71 @@
-function generate() {
-  //set password length/complexity
-  let complexity = document.getElementById("slider").nodeValue;
+var textarea = document.getElementById("password");
+var disabledBtn = document.getElementById("copy");
+var generateBtn = document.getElementById("generate");
 
+function removeClass() {
+  disabledBtn.removeAttribute("disabled");
+  if (disabledBtn.classList.contains('disabled')){
+      disabledBtn.classList.remove('disabled');
+  }
+}
+
+generateBtn.addEventListener('click', function(){
+
+
+  textarea.value='';
+
+  var passwordLength = prompt("How many characters?");
+  let allCharacters = '';
+ 
+  // keeping the password over 7 charachters
+if (passwordLength < 8) {
+  alert("Password must be at least 8 characters long");
+}
+else if (passwordLength > 128) {
+  alert("Password must be less than 128 characters");
+}
+// adding numbers into the all
+else if (prompt('Do you want numbers')) {
+  let numericCharacters = '0123456789'
+  allCharacters += numericCharacters;
+  
+}
+if (prompt('Do you want upper case letters')) {
+  allCharacters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ 
+}
+ if (prompt('Do you want lower case letters')) {
+  allCharacters += "abcdefghijklmnopqrstuvwxyz";
+  
+}
+if (prompt('Do you want special characters')){
+  allCharacters += '!@#$%^&*()';
+  
+}
+
+//function generate() {
   //possible values
-  let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-  let password = "";
+  
 
   //for loop
-  for (var i = 0; i <= complexity; i++) {
-    password =
-      password +
-      values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  for (let i = 8; i < passwordLength; i++) {
+    password +=
+      allCharacters.charAt(Math.floor(Math.random() * Math.floor(allCharacters.length - 1)));
+    console.log(password);
   }
 
-  //add password to container
-  document.getElementById("display").value = password;
-}
+  textarea.value += password;
+
+  removeClass();
+
+  disabledBtn.addEventListener('click', function(){
+    textarea.select();
+
+    document.execCommand('copy');
+
+    alert('copied to clipboard');
+
+  });
+
+})
