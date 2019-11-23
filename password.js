18aab7835caf14846b28a4b902c4f8a53bbef5a3
Josh Faison <Joshfaiso@yahoo.com>
2019-11-23 15:35:55 -0500

    final hw commit

--- password.js
+++ password.js
@@ -3,69 +3,55 @@ var disabledBtn = document.getElementById("copy");
 var generateBtn = document.getElementById("generate");
 
 function removeClass() {
-  disabledBtn.removeAttribute("disabled");
-  if (disabledBtn.classList.contains('disabled')){
-      disabledBtn.classList.remove('disabled');
-  }
-}
-
-generateBtn.addEventListener('click', function(){
-
-
-  textarea.value='';
+     disabledBtn.removeAttribute("disabled");
+   if (disabledBtn.classList.contains("disabled")) {
+     disabledBtn.classList.remove("disabled");
+   }
+ }
+//establishing an eventlistner for the button
+generateBtn.addEventListener("click", function() {
+  textarea.value = "";
 
   var passwordLength = prompt("How many characters?");
-  let allCharacters = '';
- 
+  var allCharacters = "";
+  var password = "";
+
   // keeping the password over 7 charachters
-if (passwordLength < 8) {
-  alert("Password must be at least 8 characters long");
-}
-else if (passwordLength > 128) {
-  alert("Password must be less than 128 characters");
-}
-// adding numbers into the all
-else if (prompt('Do you want numbers')) {
-  let numericCharacters = '0123456789'
-  allCharacters += numericCharacters;
-  
-}
-if (prompt('Do you want upper case letters')) {
-  allCharacters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
- 
-}
- if (prompt('Do you want lower case letters')) {
-  allCharacters += "abcdefghijklmnopqrstuvwxyz";
-  
-}
-if (prompt('Do you want special characters')){
-  allCharacters += '!@#$%^&*()';
-  
-}
-
-//function generate() {
-  //possible values
-
-  
-
-  //for loop
-  for (let i = 8; i < passwordLength; i++) {
-    password +=
-      allCharacters.charAt(Math.floor(Math.random() * Math.floor(allCharacters.length - 1)));
-    console.log(password);
+  if (passwordLength < 8) {
+    alert("Password must be at least 8 characters long");
+  } else if (passwordLength > 128) {
+    alert("Password must be less than 128 characters");
+  }
+  // adding numbers into the all
+  if (confirm("Do you want numbers")) {
+    allCharacters += "0123456789";
+  }
+  //adding uppercase letters
+  if (confirm("Do you want upper case letters")) {
+    allCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  //adding lower case letters
+  if (confirm("Do you want lower case letters")) {
+    allCharacters += "abcdefghijklmnopqrstuvwxyz";
+  }
+  //adding special charachters
+  if (confirm("Do you want special characters")) {
+    allCharacters += "!@#$%^&*()";
   }
 
-  textarea.value += password;
+  //for loop
+  for (var i = 0; i < passwordLength; i++) {
+    password += allCharacters.charAt(Math.floor(Math.random()* allCharacters.length));
+  }
+  textarea.value = password;
 
   removeClass();
 
-  disabledBtn.addEventListener('click', function(){
+  disabledBtn.addEventListener("click", function() {
     textarea.select();
 
-    document.execCommand('copy');
+    document.execCommand("copy");
 
-    alert('copied to clipboard');
-
+    alert("copied to clipboard");
   });
-
-})
+});
